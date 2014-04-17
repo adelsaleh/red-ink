dedf7f824e5bcbf450dc902874c1b681269e25ed
ams78 <ams78@192.168.192.56>
2014-04-17 17:16:33 +0300

    Added a client class, cleaned the files. All java files are to be included in the redink_src.

--- src/util/tests/src/Professor.java
+++
@@ -1,9 +0,0 @@
-public class Professor extends Person {
-	private String institute;
-	private String email;
-	public String getInstitute;
-
-	public Professor(String name, int age, String email, String institute) { }
-
-	public String getEmail() { }
-}
