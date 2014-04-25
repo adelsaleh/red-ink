ab7e7e824b07789f50189a2886292159170194f5
Bayan Rafeh <bayan.rafeh92@gmail.com>
2014-04-25 20:49:23 +0300

    Added Etag enum with list of all possible tags

---
+++ src/main/java/com/redink/ETag.java
@@ -0,0 +1,6 @@
+public enum ETag {
+    CC, CD, DT, EX, FW, IN, JJ, JJR, JJS, LS, MD, 
+    NN, NNS, NNP, NNPS, PDT, POS, PRP, PRP$, RB, RBR,
+    RBS, RP, SYM, TO, UH, VB, CBD, CBG, VBN,
+    VBP, VBZ, WDT, WP, WP$, WRB
+}
