ab2f4816aab66afd96bec27768701a3bc84939ea
Bayan Rafeh <bayan.rafeh92@gmail.com>
2014-04-27 14:16:01 +0300

    Added progress reports for work

--- src/main/java/com/redink/LocationExtractor.java
+++ src/main/java/com/redink/LocationExtractor.java
@@ -17,6 +17,7 @@ public class LocationExtractor {
 	public LocationExtractor(Novel novel) {  } 
 
 	public Iterator<Location> locationIterator() { 
+<<<<<<< Updated upstream
 	/**
 	 * EFFECTS: Retrieves all locations from novel
 	 * RETURNS: An Iterator over the locations found in the novel	
@@ -31,4 +32,20 @@ public class LocationExtractor {
 	 */
 		return false; 
 	}
+=======
+        /**
+         * EFFECTS: Loops over all the locations in this.novel.
+         * RETURNS: An iterator which loops over all the locations.
+         */
+        return null; 
+    }
+
+	public boolean isLocation(Word[] sentence) { 
+        /**
+         * EFFECTS: Checks if sentence represents a location.
+         * RETURNS: true if sentence is a location, false otherwise
+         */
+        return false; 
+    }
+>>>>>>> Stashed changes
 }
