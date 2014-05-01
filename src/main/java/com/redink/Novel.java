ed5c72d56303712b9073ae124cb103e8b716d85f
Bayan Rafeh <bayan.rafeh92@gmail.com>
2014-05-01 19:30:10 +0300

    Added getSurroundingSentence function
    Changed the return types from ArrayList to Array and modified Test cases accordingly

--- src/main/java/com/redink/Novel.java
+++ src/main/java/com/redink/Novel.java
@@ -1,7 +1,10 @@
 package com.redink;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Novel {
     /**
@@ -16,7 +19,7 @@ public class Novel {
       * Representation Invariant: words contains no punctuation and contains
       *                           uninteresting words
       */
-	private ArrayList<Word> words;
+	private Word[] words;
 
 
 	public Novel(String path) throws FileNotFoundException { 
@@ -25,16 +28,17 @@ public class Novel {
 		 * 			them in words. Words containing negative characters 
 		 * THROWS: File Not Found Exception if the file does not exist
 		 */
-		words = new ArrayList<Word>();
+		ArrayList<Word> wordList = new ArrayList<Word>();
 		Scanner reader = new Scanner(new File(path));
 		reader.useDelimiter("[^a-zA-Z\']+");
 		int offset = 0;
 		while(reader.hasNext()) {
 			String word = reader.next().toLowerCase();
-			words.add(new Word(word, offset++));
+			wordList.add(new Word(word, offset++));
 		}
 		reader.close();
-		
+		words = new Word[wordList.size()];
+		wordList.toArray(words); 
 	}
 	
 	private ArrayList<Integer> indicesOfSentence(Word[] sentence) {
@@ -44,20 +48,22 @@ public class Novel {
 		 */
 		ArrayList<Integer> indices = new ArrayList<Integer>();
 		int sentenceIndex = 0;
-		for(int i = 0; i < words.size(); i++) {
-			if(sentence[sentenceIndex].getWord().equals(words.get(i).getWord())) {
+		for(int i = 0; i < words.length; i++) {
+            if(sentenceIndex == sentence.length) {
+				indices.add(i-sentenceIndex);
+                sentenceIndex = 0;
+			}
+			if(sentence[sentenceIndex].getWord().equals(words[i].getWord())) {
 				sentenceIndex++;
 			}else{
-				sentenceIndex=0;
-			}
-			if(sentenceIndex == sentence.length-1) {
-				indices.add(i-sentenceIndex);
-			}
+                sentenceIndex = 0;
+            }
+			
 		}
-		return (indices.size() == 0) ? indices:null;
+		return indices;
 	}
 	
-	public ArrayList<Word> getSurroundingSentence(Word[] sentence, int radius) {		
+	public Word[] getSurroundingSentence(Word[] sentence, int radius) {		
 		/**
          * EFFECTS: Generates a list of words without duplicates (a duplicate is 
          * a word with the same string and offset) sorted by offset containing the 
@@ -65,11 +71,28 @@ public class Novel {
          * 
          * RETURNS: A list of words containing all the surrounding words
          */
-		return words;
-		
+        ArrayList<Integer> indices = indicesOfSentence(sentence);
+        LinkedList<Word[]> wordList = new LinkedList<Word[]>();
+        if(indices.size()==0) {
+            return new Word[]{};
+        }
+        wordList.push(getPrecedingWords(indices.get(0), radius));
+        for(int i = 0; i < indices.size()-1; i++) {
+            int index = indices.get(i);
+            int nextIndex = indices.get(i+1);
+            if(nextIndex-index > radius) {
+                wordList.push(getSucceedingWords(index+sentence.length-1, radius));
+                wordList.push(getPrecedingWords(nextIndex, radius));
+            }else{
+                wordList.push(getSucceedingWords(index+sentence.length-1, nextIndex-index-1));
+            }
+        }
+        wordList.push(getSucceedingWords(indices.get(indices.size()-1)+sentence.length-1, radius));
+        
+		return concatMulti(wordList);
 	}
 
-	public ArrayList<Word> getSurroundingWords(String w, int radius) { 
+	public Word[] getSurroundingWords(String w, int radius) { 
         /**
          * EFFECTS: Generates a list of words without duplicates (a duplicate is 
          * a word with the same string and offset) sorted by offset containing the 
@@ -80,26 +103,11 @@ public class Novel {
          * THROWS: WordNotFoundException in case the word is not found
          */
 		
-		if(radius<0) throw new IllegalArgumentException();
-		ArrayList<Word> surroundingWords = new ArrayList<Word>();
-		for(int i=0; i<words.size(); i++){
-			if(words.get(i).getWord().equals(w)){
-				for(Word word : getSurroundingWords(i, radius)) {
-					surroundingWords.add(word);
-				}
-			}
-		}
-		Collections.sort(surroundingWords);
-		for(int i=0; i<surroundingWords.size()-1; i++){
-			if(surroundingWords.get(i).equals(surroundingWords.get(i+1)) || surroundingWords.get(i).getWord().equals(w)){
-				surroundingWords.remove(i);
-				i--;
-			}
-		}
-        return surroundingWords; 
+        
+        return getSurroundingSentence(new Word[]{new Word(w)}, radius); 
     }
 
-	public ArrayList<Word> getSurroundingWords(int offset, int radius) { 
+	public Word[] getSurroundingWords(int offset, int radius) { 
         /**
          * EFFECTS: Gets the words from offset-radius to offset+radius
          *          inclusive within radius radius
@@ -108,21 +116,120 @@ public class Novel {
          *          or if offset is bigger than the size of the novel.
          */
 		if(radius<=0) throw new IllegalArgumentException();
-		ArrayList<Word> surroundingWords = new ArrayList<Word>();
-		if(offset-radius<0){
-			for(int j=0; j<=offset+radius; j++){
-				if(offset!=j) surroundingWords.add(words.get(j));
-			}
-		} else if(offset+radius>=words.size()){
-			for(int j=offset-radius; j<words.size(); j++){
-				if(offset!=j) surroundingWords.add(words.get(j));
-			}
-		} else {
-			for(int j=offset-radius; j<=offset+radius; j++){
-				if(offset!=j) surroundingWords.add(words.get(j));
+	    Word[] preceding = getPrecedingWords(offset, radius);
+        Word[] succeeding = getSucceedingWords(offset, radius);
+        
+		Word[] ret = concat(preceding, succeeding);
+
+        return ret; 
+    }
+
+    private static Word[] concatMulti(LinkedList<Word[]> wordLists) {
+        /**
+         * EFFECTS: Returns an 
+         */
+        if(wordLists.size() == 0) {
+            return new Word[0];
+        }
+        while(wordLists.size() > 1) {
+            Word[] w1 = wordLists.pop();
+            Word[] w2 = wordLists.pop();
+            wordLists.push(concat(w2, w1));
+        }
+        return wordLists.remove();
+    }
+
+    private static Word[] concat(Word[] w1, Word[] w2) {
+        /**
+         * EFFECTS: Returns an array containing all the elements
+         */
+        Word[] ret = new Word[w1.length+w2.length];
+        for(int i = 0; i < w1.length; i++) {
+            ret[i] = w1[i];
+        }
+
+        for(int i = w1.length; i < ret.length; i++) {
+            ret[i] = w2[i-w1.length];
+        }
+        return ret;
+    }
+
+    private Word[] getPrecedingWords(int offset, int radius) {
+        /**
+         * EFFECTS: Gets the words preceding offset by radius
+         * RETURNS: List of the words
+         */
+        int start = offset - radius;
+        start = (start > 0) ? start : 0;
+        Word[] preceding;
+		preceding = new Word[offset-start];
+        for(int i = start; i < offset; i++) {
+            preceding[i-start] = words[i];
+        }
+        return preceding;
+    }
+
+    private Word[] removeDuplicates(Word[] words, String removeWord) {
+        /**
+         * Removes duplicate words from words
+         */
+        if(words.length < 2) {
+            return words;
+        }
+        Word[] ret = new Word[words.length];
+        int index = 0;
+		for(int i=0; i<words.length-1; i++){
+			if(!(words[i].equals(words[i+1]))
+                        && !(words[i].getWord().equals(removeWord))){
+                ret[index]=words[i];
+                index++;
 			}
 		}
-		
-        return surroundingWords; 
+        if( !words[words.length-2].equals(words[words.length-1]) 
+                &&!words[words.length-1].getWord().equals(removeWord)) {
+            ret[index]=words[words.length-1];
+            index++;
+		}
+        return Arrays.copyOf(ret, index);
+    }
+    
+    private Word[] removeDuplicates(Word[] words) {
+        /**
+         * Removes duplicate words from words
+         */
+
+        Word[] ret = new Word[words.length];
+        int index = 0;
+		for(int i=0; i<words.length-1; i++){
+			if( !words[i].equals(words[i+1])){
+                
+                ret[index]=words[i];
+                index++;
+			}
+		}
+        if( !words[words.length-2].equals(words[words.length-1])){        
+            ret[index]=words[words.length-1];
+            index++;
+		}
+        return Arrays.copyOf(ret, index);
+    }
+
+    private Word[] getSucceedingWords(int offset, int radius) {
+        /**
+         * EFFECTS: Gets the words succeeding offset by radius
+         * RETURNS: List of the words
+         */
+        int start = offset + 1;
+        if(start >= words.length) {
+            return new Word[0];
+        }
+        
+        int end = offset+radius;
+        end = (end < words.length-1) ? end : words.length-1;
+        Word[] succeeding = new Word[end+1-start];
+        for(int i = start; i <= end; i++) {
+            succeeding[i-start] = words[i];
+        }
+        return succeeding;
     }
 }
--- src/test/java/com/redink/NovelTest.java
+++ src/test/java/com/redink/NovelTest.java
@@ -3,7 +3,7 @@ package com.redink;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.Ignore;
 import junit.framework.TestCase;
 
 import java.io.FileNotFoundException;
@@ -32,30 +32,67 @@ public class NovelTest extends TestCase {
     	
     }
     
+    public void testGetSurroundingWordsOfSentence() {
+        String[] words = new String[] {"the", "once", "upon", "a", "time", "there", "were",
+                                       "who","left","their","happier", "than", 
+                                       "the", "and", "they", "easily",
+                                        "to", "rain", "the","started", "to", "feel",
+                                        "an", "lnch", "the", "watched", "him", "and"
+                                       };
+        Word[] sentence = {new Word("three"), new Word("little"), new Word("pigs")};
+        Word[] surroundingWords = n.getSurroundingSentence(sentence, 3);
+		String[] cmp = new String[surroundingWords.length];
+        System.out.println(Arrays.toString(surroundingWords));
+        for(int i=0; i<surroundingWords.length; i++){
+        	cmp[i] = surroundingWords[i].getWord();
+        }
+        assertTrue(Arrays.equals(cmp, words));	
+        words = new String[] {"the", "three", "little", "pigs", 
+                              "once", "upon", "a", "time", "there", "were",
+                              "who","left","their","mummy", "and", "daddy", "to",
+                              "having","fun", "none", "were", "happier",
+                              "than", "the", "and", "they", "easily", "made",
+                              "friends", "with", "everyone", "came", "and", "it",
+                              "began", "to", "rain", "the", "started", "to", "feel",
+                              "they", "needed", "a", "real", "house", "did", "not",
+                              "budge", "an", "lnch", "the", "watched", "him", "and",
+                              "their", "fear", "began", "to"
+                             };
+        sentence = new Word[]{new Word("three"), new Word("little"), new Word("pigs")};
+        surroundingWords = n.getSurroundingSentence(sentence, 7);
+		cmp = new String[surroundingWords.length];
+        for(int i=0; i<surroundingWords.length; i++){
+        	cmp[i] = surroundingWords[i].getWord();
+        }
+    }
     @Test
     public void testGetSurroundingWords() {
         String[] words = new String[] {"the", "three", "little", "pigs", "once", "a", "time", "there", "were", "three", "little"};
-        String[] surroundingWords = new String[words.length];
+        Word[] surroundingWords = n.getSurroundingWords(5, 6);
+		String[] cmp = new String[surroundingWords.length];
         for(int i=0; i<words.length; i++){
-        	surroundingWords[i] = n.getSurroundingWords(5, 6).get(i).getWord();
+        	cmp[i] = surroundingWords[i].getWord();
         }
-        assertTrue(Arrays.equals(surroundingWords, words));
+        assertTrue(Arrays.equals(cmp, words));
         
         
         words = new String []{"the", "three", "little", "pigs", "once","upon", "time", "there", "were", "three", "little" , "pigs"};
-        surroundingWords = new String[words.length];
+        surroundingWords = n.getSurroundingWords(6, 6);
+		cmp = new String[surroundingWords.length];
         for(int i=0; i<words.length; i++){
-        	surroundingWords[i] = n.getSurroundingWords(6, 6).get(i).getWord();
+        	cmp[i] = surroundingWords[i].getWord();
         }
-        assertTrue(Arrays.equals(surroundingWords, words));
+        assertTrue(Arrays.equals(cmp, words));
 
         
         words = new String []{"once", "upon", "a", "there", "were", "three"};
-        surroundingWords = new String[words.length];
+        surroundingWords = n.getSurroundingWords(7, 3);
+	    cmp = new String[surroundingWords.length];
+
         for(int i=0; i<words.length; i++){
-        	surroundingWords[i] = n.getSurroundingWords(7, 3).get(i).getWord();
+        	cmp[i] = surroundingWords[i].getWord();
         }
-        assertTrue(Arrays.equals(surroundingWords, words));
+        assertTrue(Arrays.equals(cmp, words));
 
         
         words = new String[]{"a"};
@@ -70,14 +107,14 @@ public class NovelTest extends TestCase {
     public void testGetSurroundingWordsByWord() {
         String[] words = new String[] {"of", "planks", "of", "seasoned", "wood", "it", "took", "him", "two", "days"};
         String[] surroundingWords = new String[words.length];
-        ArrayList<Word> ws = n.getSurroundingWords("clunk", 5);
+        Word[] ws = n.getSurroundingWords("clunk", 5);
         for(int i=0; i<words.length; i++){
-        	surroundingWords[i] = ws.get(i).getWord();
+        	surroundingWords[i] = ws[i].getWord();
         }
         System.out.println(Arrays.toString(surroundingWords));
         assertTrue(Arrays.equals(surroundingWords, words));
         
-        assertTrue(n.getSurroundingWords("hdffsdgsdgsd", 5).size() == 0);
+        assertTrue(n.getSurroundingWords("hdffsdgsdgsd", 5).length == 0);
     }
     
 }
